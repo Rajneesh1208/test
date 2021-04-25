70a2111775b127c7859bb42101a5823659704a7a
PRAVEEN KUMAR MISHRA <rajaneeshmishra0262@gmail.com>
2021-04-25 17:10:29 +0530

    added

---
+++ test.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class test1 {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the value of n");
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		System.out.println("enter the value of array elements");
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		System.out.println("array elements are...");
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(a[i]+"");
+		}
+	}
+
+}
